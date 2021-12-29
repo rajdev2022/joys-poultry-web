65e5bb8c863d53afbb5c808215c4f1ccb40b6bef
manishbharatham <manish.bharathamitsolutions@gmail.com>
2021-12-29 13:18:30 +0530

    some comment has added

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,6 +2,8 @@ var base_url = "https://digi.bharathamitsolutions.com/poultry-demo/wp-admin/admi
 //var base_url = "http://localhost/poultry-demo/wp-admin/admin-ajax.php";
 
 //var base_url = "http://192.168.1.11/poultry-demo/wp-admin/admin-ajax.php";
+// some comment adde
+
 
 var init_show_password_click = 0;
 var page_loading_delay_time = 100;
